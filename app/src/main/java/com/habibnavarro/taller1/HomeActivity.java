ffd25cc1f401335062d5129fc6e59ca5a4303fc9
habib n <habnacos@gmail.com>
2021-03-29 23:14:30 -0500

    Bottom navigation

--- app/src/main/java/com/habibnavarro/taller1/HomeActivity.java
+++ app/src/main/java/com/habibnavarro/taller1/HomeActivity.java
@@ -1,14 +1,52 @@
 package com.habibnavarro.taller1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+    FirstFragment firstFragment = new FirstFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
+
+    BottomNavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        navigation = findViewById(R.id.bottom_navigation);
+
+        navigation.getMenu().getItem(1).setChecked(true);
+        loadFragment(secondFragment);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.firstFragment:
+                        loadFragment(firstFragment);
+                        return true;
+                    case R.id.secondFragment:
+                        loadFragment(secondFragment);
+                        return true;
+                    case R.id.thirdFragment:
+                        loadFragment(thirdFragment);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
     }
 }
